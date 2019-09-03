a6ad1474f8d9883a7c1b244fdd2589918de7cc05
Priscilla Valim <priscilla.valim@outlook.com>
2019-09-03 19:41:38 -0300

    Correção

--- If/Ex.2.cpp
+++ If/Ex.2.cpp
@@ -1,29 +1,27 @@
-#include <iostream.h>
-#include <conio.h>
-#include <math.h>
-void main()
+#include <iostream>
+
+int main()
 {
     float nota1, nota2, media;
-    clrscr();
     cout << "\nDigite a nota 1:";
     cin >> nota1;
     cout << "\nDigite a nota 2:";
-    cin >> nota2;
+    cin >> nota2; 
 
-    media = nota1 + nota2 / 2;
+    media = nota1 + nota2/2; 
     if (media >= 7)
     {
-        cout << "\nAprovado";
+        cout << "\nAprovado"; 
     }
-
+    
     else if (media < 7 && media <= 4)
     {
         cout << "\nExame";
     }
-
-    else
+    
+    else 
     {
         cout << "\nReprovado";
-    }
-    getch();
+    } 
+    return 0;
 }
